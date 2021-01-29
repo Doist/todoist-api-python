a8c73fa5e98e5afa4d75dde6080528d6e54f30fc
David Fishlock <email@davidfishlock.com>
2021-01-29 16:44:22 +0000

    Add async API client

--- todoist_api_python/api.py
+++ todoist_api_python/api.py
@@ -5,7 +5,6 @@ import requests
 from todoist_api_python.endpoints import TASKS_ENDPOINT, get_rest_url
 from todoist_api_python.http_requests import delete, get, post
 from todoist_api_python.models import Task
-from todoist_api_python.utils import async_wrap
 
 
 class TodoistAPI:
@@ -18,8 +17,6 @@ class TodoistAPI:
         task = get(self._session, self._token, endpoint)
         return Task.from_dict(task)
 
-    get_task_async = async_wrap(get_task)
-
     def get_tasks(self, **kwargs) -> List[Task]:
         ids = kwargs.pop("ids", None)
 
@@ -30,8 +27,6 @@ class TodoistAPI:
         tasks = get(self._session, self._token, endpoint, kwargs)
         return [Task.from_dict(obj) for obj in tasks]
 
-    get_tasks_async = async_wrap(get_tasks)
-
     def add_task(self, content: str, **kwargs) -> Task:
         endpoint = get_rest_url(TASKS_ENDPOINT)
         data: Dict[str, Any] = {"content": content}
@@ -39,32 +34,22 @@ class TodoistAPI:
         task = post(self._session, self._token, endpoint, data=data)
         return Task.from_dict(task)
 
-    add_task_async = async_wrap(add_task)
-
     def update_task(self, task_id: str, **kwargs) -> Task:
         endpoint = get_rest_url(f"{TASKS_ENDPOINT}/{task_id}")
         task = post(self._session, self._token, endpoint, data=kwargs)
         return Task.from_dict(task)
-
-    update_task_async = async_wrap(update_task)
 
     def close_task(self, task_id: str, **kwargs) -> bool:
         endpoint = get_rest_url(f"{TASKS_ENDPOINT}/{task_id}/close")
         success = post(self._session, self._token, endpoint, data=kwargs)
         return success
 
-    close_task_async = async_wrap(close_task)
-
     def reopen_task(self, task_id: str, **kwargs) -> bool:
         endpoint = get_rest_url(f"{TASKS_ENDPOINT}/{task_id}/reopen")
         success = post(self._session, self._token, endpoint, data=kwargs)
         return success
 
-    reopen_task_async = async_wrap(reopen_task)
-
     def delete_task(self, task_id: str, **kwargs) -> bool:
         endpoint = get_rest_url(f"{TASKS_ENDPOINT}/{task_id}")
         success = delete(self._session, self._token, endpoint, args=kwargs)
         return success
-
-    delete_task_async = async_wrap(delete_task)
---
+++ todoist_api_python/api_async.py
@@ -0,0 +1,36 @@
+import asyncio
+from typing import List
+
+from todoist_api_python import TodoistAPI
+from todoist_api_python.models import Task
+
+
+async def run_async(func):
+    loop = asyncio.get_event_loop()
+    return await loop.run_in_executor(None, func)
+
+
+class TodoistAPIAsync:
+    def __init__(self, token: str):
+        self._api = TodoistAPI(token)
+
+    async def get_task(self, task_id: int) -> Task:
+        return await run_async(lambda: self._api.get_task(task_id))
+
+    async def get_tasks(self, **kwargs) -> List[Task]:
+        return await run_async(lambda: self._api.get_tasks(**kwargs))
+
+    async def add_task(self, content: str, **kwargs) -> Task:
+        return await run_async(lambda: self._api.add_task(content, **kwargs))
+
+    async def update_task(self, task_id: str, **kwargs) -> Task:
+        return await run_async(lambda: self._api.update_task(task_id, **kwargs))
+
+    async def close_task(self, task_id: str, **kwargs) -> bool:
+        return await run_async(lambda: self._api.close_task(task_id, **kwargs))
+
+    async def reopen_task(self, task_id: str, **kwargs) -> bool:
+        return await run_async(lambda: self._api.reopen_task(task_id, **kwargs))
+
+    async def delete_task(self, task_id: str, **kwargs) -> bool:
+        return await run_async(lambda: self._api.delete_task(task_id, **kwargs))
