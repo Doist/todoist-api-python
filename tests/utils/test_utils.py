aee3b29e862da7fa50b270bd5392162deed48190
Lefteris Chatzimparmpas <lefteris@doist.com>
2022-08-29 12:29:04 +0300

    Remove forgotten unused function

--- tests/utils/test_utils.py
+++ tests/utils/test_utils.py
@@ -1,9 +1,6 @@
 import re
 from typing import Callable, Optional
 
-import pytest
-import responses
-
 from tests.data.test_defaults import DEFAULT_REQUEST_ID, DEFAULT_TOKEN
 from todoist_api_python.api import TodoistAPI
 
@@ -16,19 +13,6 @@ def assert_auth_header(request):
 
 def assert_request_id_header(request):
     assert request.headers["X-Request-Id"] == DEFAULT_REQUEST_ID
-
-
-def assert_id_validation(func: Callable, requests_mock: responses.RequestsMock):
-    requests_mock.assert_all_requests_are_fired = False
-
-    requests_mock.add(responses.GET, MATCH_ANY_REGEX)
-    requests_mock.add(responses.POST, MATCH_ANY_REGEX)
-    requests_mock.add(responses.DELETE, MATCH_ANY_REGEX)
-
-    with pytest.raises(ValueError):
-        func()
-
-    assert len(requests_mock.calls) == 0
 
 
 def get_todoist_api_patch(method: Optional[Callable]) -> str:
