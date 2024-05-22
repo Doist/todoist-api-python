69f8896ca90efebaf4f791c960db7579ca4d56ca
Ted Yeates <t3d.y34t3s@gmail.com>
2024-05-22 07:35:03 +0100

    fix: key error on can_assign_tasks in Project model (#137)

--- todoist_api_python/models.py
+++ todoist_api_python/models.py
@@ -34,7 +34,7 @@ class Project:
             is_inbox_project=obj.get("is_inbox_project"),
             is_shared=obj["is_shared"],
             is_team_inbox=obj.get("is_team_inbox"),
-            can_assign_tasks=obj["can_assign_tasks"],
+            can_assign_tasks=obj.get("can_assign_tasks"),
             name=obj["name"],
             order=obj.get("order"),
             parent_id=obj.get("parent_id"),
