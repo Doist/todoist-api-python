6ab7e2a7ab98b7b1c2ead7f19c1ac201e3d9d00c
Lefteris Chatzimparmpas <lefteris@doist.com>
2024-08-07 13:49:33 +0300

    feat: TodoistAPIAsync accepts a session

--- tests/test_api_async.py
+++ tests/test_api_async.py
@@ -2,6 +2,8 @@ from __future__ import annotations
 
 from unittest.mock import MagicMock, patch
 
+import requests
+
 from tests.data.test_defaults import DEFAULT_TOKEN
 from tests.utils.test_utils import get_todoist_api_patch
 from todoist_api_python.api import TodoistAPI
@@ -13,4 +15,12 @@ def test_constructs_api_with_token(sync_api_constructor: MagicMock):
     sync_api_constructor.return_value = None
     TodoistAPIAsync(DEFAULT_TOKEN)
 
-    sync_api_constructor.assert_called_once_with(DEFAULT_TOKEN)
+    sync_api_constructor.assert_called_once_with(DEFAULT_TOKEN, None)
+
+
+@patch(get_todoist_api_patch(TodoistAPI.__init__))
+def test_constructs_api_with_token_and_session(sync_api_constructor: MagicMock):
+    sync_api_constructor.return_value = None
+    session = requests.Session()
+    TodoistAPIAsync(DEFAULT_TOKEN, session)
+    sync_api_constructor.assert_called_once_with(DEFAULT_TOKEN, session)
